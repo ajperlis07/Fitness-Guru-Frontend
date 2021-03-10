d26ed491b9cd8b590888b625557316b9e040c4ed
ajperlis07 <ajperlis07@gmail.com>
2021-03-09 19:26:53 -0500

    Working on getting exercises to display

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const routinesList= document.querySelector("ul#routines-list")
-const 
+const routinesList= document.querySelector("ol#routines-list")
+const routineItem = document.querySelector('li[data-id]')
+ 
 
 function renderRoutine(routine){
-    const li= document.createElement("li")
+    const li = document.createElement("li")
 
-    li.textContent= routine.name
-    li.dataset.id= routine.id
+    li.textContent = routine.name
+    li.dataset.id = routine.id
 
     routinesList.append(li)
 }
@@ -15,6 +16,22 @@ function renderRoutines(routines){
         renderRoutine(routine)
     })
 }
+
+routinesList.addEventListener('click', event => {
+    if (event.target.tagName === 'LI')
+    fetch (`http://localhost:3000/routines/${event.target.dataset.id}`)
+        .then(res => res.json())
+        .then(exercise => {
+            const exerciseImage = document.querySelector('img')
+            const exerciseTitle = document.querySelector('h1.title')
+            const workoutNotes = document.querySelector('p.notes')
+
+            exerciseImage.src = exercise.image 
+            exerciseImage.alt = exercise.title
+            exerciseTitle.textContent = exercise.exercises.title
+            workoutNotes.textContent = exercise.exercises.workouts.notes
+        }) 
+    })
 
 fetch('http://localhost:3000/routines')
 .then(response => response.json())
