2273cc3aa5141a64bb167a26765ed7a809a1c797
github username <github email address>
2021-03-10 13:20:49 -0500

    working exercise card shows

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const routinesList= document.querySelector("ol#routines-list")
+const routinesList = document.querySelector("ol#routines-list")
 const routineItem = document.querySelector('li[data-id]')
- 
+const section = document.querySelector("section")
+const newRoutine= document.querySelector("form#new-routine-form")
 
-function renderRoutine(routine){
+
+function renderRoutine(routine) {
     const li = document.createElement("li")
 
     li.textContent = routine.name
@@ -11,29 +13,47 @@ function renderRoutine(routine){
     routinesList.append(li)
 }
 
-function renderRoutines(routines){
-    routines.forEach (function(routine){
+function renderRoutines(routines) {
+    routines.forEach(function (routine) {
         renderRoutine(routine)
     })
 }
 
 routinesList.addEventListener('click', event => {
-    if (event.target.tagName === 'LI')
-    fetch (`http://localhost:3000/routines/${event.target.dataset.id}`)
-        .then(res => res.json())
-        .then(exercise => {
-            const exerciseImage = document.querySelector('img')
-            const exerciseTitle = document.querySelector('h1.title')
-            const workoutNotes = document.querySelector('p.notes')
+    if (event.target.tagName === 'LI') {
+        fetch(`http://localhost:3000/routines/${event.target.dataset.id}`)
+            .then(res => res.json())
+            .then(routineObj => {
+                section.innerHTML = ""
+                routineObj.exercises.forEach(function (exercise) {
+                    
 
-            exerciseImage.src = exercise.image 
-            exerciseImage.alt = exercise.title
-            exerciseTitle.textContent = exercise.exercises.title
-            workoutNotes.textContent = exercise.exercises.workouts.notes
-        }) 
-    })
+                    const div = document.createElement("div")
+                    div.dataset.id = exercise.id
+
+                    div.classList.add("exercise-card")
+
+                    div.innerHTML = `
+                    <h1 class="title">${exercise.title}</h1>
+            <img src="${exercise.image}" alt="${exercise.title}">
+          `
+                    exercise.workouts.forEach(function (workout) {
+                        const ptag = document.createElement("p")
+                        ptag.classList.add("notes")
+
+                        ptag.textContent = `Notes: ${workout.notes}`
+
+                        div.append(ptag)
+                    })
+                    section.append(div)
+                })
+            })
+    }
+})
+
+
 
 fetch('http://localhost:3000/routines')
-.then(response => response.json())
-.then(routine => renderRoutines(routine))
+    .then(response => response.json())
+    .then(routine => renderRoutines(routine))
 
